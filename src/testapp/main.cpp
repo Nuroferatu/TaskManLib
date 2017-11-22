fa3e3c53b63fc0e91de4c7a76c177e8067c1c8ea
Nuroferatu <32645518+Nuroferatu@users.noreply.github.com>
2017-11-22 19:13:07 +0100

    Added async task manager class

--- src/testapp/main.cpp
+++ src/testapp/main.cpp
@@ -1,9 +1,16 @@
 #include <Windows.h>
 #include <iostream>
+#include "taskman.h"
 
 using namespace std;
 
+
 int main() {
+    TaskMan     taskManager;
+
+    taskManager.onInit( 2 );
+
+    taskManager.onShutdown();
 }
 
 /* EOF */
